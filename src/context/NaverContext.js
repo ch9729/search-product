f9063b5a96d90f3471484b95693aa1dcfbbe2682
ch9729 <karlll@naver.com>
2025-01-15 09:24:59 +0900

    proxy 변경

--- src/context/NaverContext.js
+++ src/context/NaverContext.js
@@ -5,8 +5,13 @@ export const ApiContext = createContext();
 export const NaverContext = ({ children }) => {
   const [searchs, setSearchs] = useState([]);
 
+  const API_ABSE_URL =
+    process.env.NODE_ENV === "production"
+      ? "https://openapi.naver.com"
+      : "/api";
+
   const getSearch = (product) => {
-    fetch(`/api/v1/search/shop.json?query=${product}`, {
+    fetch(`${API_ABSE_URL}/v1/search/shop.json?query=${product}`, {
       headers: {
         "X-Naver-Client-Id": process.env.REACT_APP_X_Naver_Client_Id,
         "X-Naver-Client-Secret": process.env.REACT_APP_X_Naver_Client_Secret,
