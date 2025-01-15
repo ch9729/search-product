0adca0b73cd424f75c661715983085c86f2949d5
ch9729 <karlll@naver.com>
2025-01-15 09:30:40 +0900

    오타수정

--- src/context/NaverContext.js
+++ src/context/NaverContext.js
@@ -5,20 +5,20 @@ export const ApiContext = createContext();
 export const NaverContext = ({ children }) => {
   const [searchs, setSearchs] = useState([]);
 
-  const API_ABSE_URL =
+  const API_BASE_URL =
     process.env.NODE_ENV === "production"
       ? "https://openapi.naver.com"
       : "/api";
 
   const getSearch = (product) => {
-    fetch(`${API_ABSE_URL}/v1/search/shop.json?query=${product}`, {
+    fetch(`${API_BASE_URL}/v1/search/shop.json?query=${product}`, {
       headers: {
         "X-Naver-Client-Id": process.env.REACT_APP_X_Naver_Client_Id,
         "X-Naver-Client-Secret": process.env.REACT_APP_X_Naver_Client_Secret,
         Accept: "application/json",
       },
     })
-      .then((res) => res.json())
+      .then((res) => console.log(res))
       .then((data) => {
         setSearchs(data.items);
       })
